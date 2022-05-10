36b810bddcc0c93d97192d882abd6c0da6649808
emascapa <emascapa@gmail.com>
2022-05-10 12:23:26 +0200

    milestone 3

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,15 @@
       </nav>
     </header>
     <main>
+      <font-awesome-icon icon="fa-solid fa-star" /><font-awesome-icon
+        icon="fa-solid fa-star"
+      />
+      <font-awesome-icon icon="fa-regular fa-star" />
+      <font-awesome-icon icon="fa-regular fa-star" />
       <div v-if="filmList.length > 0" class="box">
         <h2>MOVIES</h2>
         <div v-for="(item, index) in filmList" :key="index" class="movie">
+          <img :src="item.poster_path" :alt="item.original_title" />
           <ul>
             <li>{{ item.title }}</li>
             <li>{{ item.original_title }}</li>
@@ -20,12 +26,22 @@
               <country-flag
                 v-if="item.original_language != null"
                 :country="item.original_language"
-                size="medium"
               />
               <span v-else>ND</span>
             </li>
-            <!-- {{ item.original_language }} -->
             <li>{{ item.vote_average }}</li>
+            <li>
+              <font-awesome-icon
+                v-for="i in item.vote_average"
+                :key="i"
+                icon="fa-solid fa-star"
+              />
+              <font-awesome-icon
+                v-for="n in 5 - item.vote_average"
+                :key="n"
+                icon="fa-regular fa-star"
+              />
+            </li>
           </ul>
         </div>
       </div>
@@ -34,6 +50,7 @@
       <div v-if="serieList.length > 0" class="box">
         <h2>SERIES</h2>
         <div v-for="(item, index) in serieList" :key="index" class="movie">
+          <img :src="item.poster_path" :alt="item.original_title" />
           <ul>
             <li>{{ item.name }}</li>
             <li>{{ item.original_name }}</li>
@@ -41,12 +58,23 @@
               <country-flag
                 v-if="item.origin_country[0] != null"
                 :country="item.origin_country[0]"
-                size="medium"
               />
               <span v-else>ND</span>
             </li>
             <!-- {{ item.original_language }} -->
             <li>{{ item.vote_average }}</li>
+            <li>
+              <font-awesome-icon
+                v-for="i in item.vote_average"
+                :key="i"
+                icon="fa-solid fa-star"
+              />
+              <font-awesome-icon
+                v-for="n in 5 - item.vote_average"
+                :key="n"
+                icon="fa-regular fa-star"
+              />
+            </li>
           </ul>
         </div>
       </div>
@@ -73,8 +101,6 @@ export default {
       filmList: [],
       serieList: [],
       apiKey: "8166a56c9ae51f70996c4ce9734e81c3",
-      //apiMovieLink: `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&language=en-US&page=1&include_adult=false`,
-      //apiMovieLink: `https://api.themoviedb.org/3/search/movie?language=it-IT&page=1&api_key=${this.apiKey}&include_adult=false`,
       apiMovieLink: `https://api.themoviedb.org/3/search/movie?page=1&api_key=8166a56c9ae51f70996c4ce9734e81c3&language=it-IT&include_adult=false`,
       apiSerieLink: `https://api.themoviedb.org/3/search/tv?page=1&api_key=8166a56c9ae51f70996c4ce9734e81c3&language=it-IT&include_adult=false`,
     };
@@ -84,7 +110,6 @@ export default {
       if (this.inputString.replace(/\s+/g, "") !== "") {
         let queryString = this.inputString.replace(/\s+/g, " ");
         queryString = queryString.replace(" ", "+");
-        //const queryString = this.inputString;
 
         console.log(`${this.apiMovieLink}&query=${queryString}`);
 
@@ -94,6 +119,12 @@ export default {
             this.filmList = response.data.results;
 
             this.makeFlags();
+
+            this.makePosters(this.filmList);
+
+            this.roundVotes(this.filmList);
+
+            //this.makeStars(this.filmList);
 
             console.log(this.filmList);
           })
@@ -107,6 +138,12 @@ export default {
             this.serieList = response.data.results;
 
             //this.makeFlags();
+
+            this.makePosters(this.serieList);
+
+            this.roundVotes(this.serieList);
+
+            //this.makeStars(this.serieList);
 
             console.log(this.serieList);
           })
@@ -126,24 +163,55 @@ export default {
           item.original_language = "us";
         } else if (item.original_language == "ja") {
           item.original_language = "jp";
+        } else if (item.original_language == "zh") {
+          item.original_language = "cn";
         }
       });
     },
-    /*    searchMovies() {
-      const queryString = state.searchInput.replace(" ", "+");
-
-      axios
-        .get(`${this.apiMovieLink}&query=${queryString}`)
-        .then((response) => {
-          this.filmList = response.data.results;
-          //this.loading = false;
-          console.log(this.filmList);
-        })
-        .catch((error) => {
-          console.log(error);
-          error;
-          this.error = `Sorry Error found: ${error}`;
-        });
+    makePosters(arrayName) {
+      arrayName.forEach((item) => {
+        if (item.poster_path != null) {
+          item.poster_path =
+            "https://image.tmdb.org/t/p/w500/" + item.poster_path;
+        } else {
+          item.poster_path =
+            "https://www.tube-culture.com/images/titles_cache/750x1031_movienophoto_2014_1000x1375.jpg";
+        }
+      });
+    },
+    roundVotes(arrayName) {
+      arrayName.forEach((item) => {
+        item.vote_average = Math.round(parseFloat(item.vote_average) * 0.5);
+      });
+    },
+/*     makeStars(arrayName) {
+      arrayName.forEach((item) => {
+        switch (item.vote_average) {
+          case 0:
+            item.stars =
+              '<font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" />';
+            break;
+          case 1:
+            item.stars =
+              '<font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" />';
+            break;
+          case 2:
+            item.stars =
+              '<font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" />';
+            break;
+          case 3:
+            item.stars =
+              '<font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-regular fa-star" /> <font-awesome-icon icon="fa-regular fa-star" />';
+            break;
+          case 4:
+            item.stars =
+              '<font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-regular fa-star" />';
+            break;
+          case 5:
+            item.stars =
+              '<font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" /> <font-awesome-icon icon="fa-solid fa-star" />';
+        }
+      });
     }, */
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+/* FLAGS-EXTENCION */
 import CountryFlag from 'vue-country-flag'
 
 Vue.component('country-flag', CountryFlag)
+
+/* FONT-AWESOME */
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import { faStar as faStarSolid} from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular} from '@fortawesome/free-regular-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(faStarSolid, faStarRegular) 
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
